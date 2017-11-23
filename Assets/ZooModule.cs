a671e39533b3897dd2cfbdb6c320efb2366342c1
Timwi <github@timwi.de>
2017-11-23 20:16:33 +0100

    Update for next KTANE version.

--- Assets/ZooModule.cs
+++ Assets/ZooModule.cs
@@ -23,11 +23,12 @@ public class ZooModule : MonoBehaviour
     public GameObject PedestalsParent;
     public GameObject Animal;
     public Mesh PlaneMesh;
+    public Texture[] AnimalImages;
 
     private KMSelectable[] _pedestals;
     private int[] _pedestalChildIndexes;
-    private AnimalInfo[] _solutionLine;
-    private AnimalInfo[] _selection;    // what’s actually shown on the module once the door opens
+    private string[] _solutionLine;
+    private string[] _selection;    // what’s actually shown on the module once the door opens
     private int[] _selectionPedestalIxs;    // what’s actually shown on the module once the door opens
 
     enum State { DoorClosed, DoorOpen, DoorClosing, Solved }
@@ -35,6 +36,76 @@ public class ZooModule : MonoBehaviour
 
     private static int _moduleIdCounter = 1;
     private int _moduleId;
+
+    private static Dictionary<Hex, string> _inGrid;
+    private static List<string> _outGridQ = new List<string> { "Gazelle", "Caracal", "Cheetah", "Ocelot", "Sheep", "Caterpillar", "Groundhog", "Armadillo", "Orca" };
+    private static List<string> _outGridR = new List<string> { "Plesiosaur", "Penguin", "Baboon", "Whale", "Squid", "Coyote", "Ram", "Deer", "Crocodile" };
+
+    static ZooModule()
+    {
+        _inGrid = new Dictionary<Hex, string>();
+        _inGrid[new Hex(0, -4)] = "Cow";
+        _inGrid[new Hex(1, -4)] = "Tyrannosaurus Rex";
+        _inGrid[new Hex(2, -4)] = "Rabbit";
+        _inGrid[new Hex(3, -4)] = "Horse";
+        _inGrid[new Hex(4, -4)] = "Flamingo";
+        _inGrid[new Hex(-1, -3)] = "Cat";
+        _inGrid[new Hex(0, -3)] = "Bat";
+        _inGrid[new Hex(1, -3)] = "Ant";
+        _inGrid[new Hex(2, -3)] = "Fly";
+        _inGrid[new Hex(3, -3)] = "Llama";
+        _inGrid[new Hex(4, -3)] = "Hyena";
+        _inGrid[new Hex(-2, -2)] = "Pig";
+        _inGrid[new Hex(-1, -2)] = "Owl";
+        _inGrid[new Hex(0, -2)] = "Rhinoceros";
+        _inGrid[new Hex(1, -2)] = "Tortoise";
+        _inGrid[new Hex(2, -2)] = "Sea Horse";
+        _inGrid[new Hex(3, -2)] = "Camel";
+        _inGrid[new Hex(4, -2)] = "Dimetrodon";
+        _inGrid[new Hex(-3, -1)] = "Spider";
+        _inGrid[new Hex(-2, -1)] = "Goose";
+        _inGrid[new Hex(-1, -1)] = "Snail";
+        _inGrid[new Hex(0, -1)] = "Monkey";
+        _inGrid[new Hex(1, -1)] = "Wolf";
+        _inGrid[new Hex(2, -1)] = "Kangaroo";
+        _inGrid[new Hex(3, -1)] = "Lobster";
+        _inGrid[new Hex(4, -1)] = "Dromedary";
+        _inGrid[new Hex(-4, 0)] = "Bear";
+        _inGrid[new Hex(-3, 0)] = "Dragonfly";
+        _inGrid[new Hex(-2, 0)] = "Butterfly";
+        _inGrid[new Hex(-1, 0)] = "Fox";
+        _inGrid[new Hex(0, 0)] = "Dolphin";
+        _inGrid[new Hex(1, 0)] = "Eagle";
+        _inGrid[new Hex(2, 0)] = "Porcupine";
+        _inGrid[new Hex(3, 0)] = "Otter";
+        _inGrid[new Hex(4, 0)] = "Warthog";
+        _inGrid[new Hex(-4, 1)] = "Ferret";
+        _inGrid[new Hex(-3, 1)] = "Lion";
+        _inGrid[new Hex(-2, 1)] = "Squirrel";
+        _inGrid[new Hex(-1, 1)] = "Giraffe";
+        _inGrid[new Hex(0, 1)] = "Koala";
+        _inGrid[new Hex(1, 1)] = "Crab";
+        _inGrid[new Hex(2, 1)] = "Frog";
+        _inGrid[new Hex(3, 1)] = "Swallow";
+        _inGrid[new Hex(-4, 2)] = "Stegosaurus";
+        _inGrid[new Hex(-3, 2)] = "Pterodactyl";
+        _inGrid[new Hex(-2, 2)] = "Cobra";
+        _inGrid[new Hex(-1, 2)] = "Hippopotamus";
+        _inGrid[new Hex(0, 2)] = "Triceratops";
+        _inGrid[new Hex(1, 2)] = "Duck";
+        _inGrid[new Hex(2, 2)] = "Starfish";
+        _inGrid[new Hex(-4, 3)] = "Elephant";
+        _inGrid[new Hex(-3, 3)] = "Rooster";
+        _inGrid[new Hex(-2, 3)] = "Woodpecker";
+        _inGrid[new Hex(-1, 3)] = "Apatosaurus";
+        _inGrid[new Hex(0, 3)] = "Beaver";
+        _inGrid[new Hex(1, 3)] = "Gorilla";
+        _inGrid[new Hex(-4, 4)] = "Mouse";
+        _inGrid[new Hex(-3, 4)] = "Seal";
+        _inGrid[new Hex(-2, 4)] = "Skunk";
+        _inGrid[new Hex(-1, 4)] = "Viper";
+        _inGrid[new Hex(0, 4)] = "Salamander";
+    }
 
     void Start()
     {
@@ -96,8 +167,8 @@ public class ZooModule : MonoBehaviour
         }).Where(h => h != null);
 
         var inf = hexes.PickRandom();
-        Debug.LogFormat("[Zoo #{0}] Animals shown on front: {1}, {2}", _moduleId, Data.Qs[inf.Line[0].Q + 4].Name, Data.Rs[inf.Line[0].R + 4].Name);
-        Debug.LogFormat("[Zoo #{0}] Line: {1} ({2})", _moduleId, inf.Line.Select(h => Data.Hexes[h].Name).JoinString(", "), inf.PortType == null ? "two-steps rule" : inf.PortType.ToString());
+        Debug.LogFormat("[Zoo #{0}] Animals shown on front: {1}, {2}", _moduleId, _outGridQ[inf.Line[0].Q + 4], _outGridR[inf.Line[0].R + 4]);
+        Debug.LogFormat("[Zoo #{0}] Line: {1} ({2})", _moduleId, inf.Line.Select(h => _inGrid[h]).JoinString(", "), inf.PortType == null ? "two-steps rule" : inf.PortType.ToString());
 
         _pedestalChildIndexes = new int[_pedestals.Length];
         for (int i = 0; i < _pedestals.Length; i++)
@@ -108,11 +179,11 @@ public class ZooModule : MonoBehaviour
 
         // Put the coordinate animals on the front of the door
         var mult = Rnd.Range(0, 2) == 0 ? -1 : 1;
-        CreateGraphic(Data.Qs[inf.Line[0].Q + 4], .35f * mult, -.0499f, -.35f * mult, .07f, Door.transform);
-        CreateGraphic(Data.Rs[inf.Line[0].R + 4], -.35f * mult, -.0499f, .35f * mult, .07f, Door.transform);
+        CreateGraphic(_outGridQ[inf.Line[0].Q + 4], .35f * mult, -.0499f, -.35f * mult, .07f, Door.transform);
+        CreateGraphic(_outGridR[inf.Line[0].R + 4], -.35f * mult, -.0499f, .35f * mult, .07f, Door.transform);
 
         // Remember the solution line
-        _solutionLine = inf.Line.Select(h => Data.Hexes[h]).ToArray();
+        _solutionLine = inf.Line.Select(h => _inGrid[h]).ToArray();
 
         Door.OnInteract = delegate
         {
@@ -123,19 +194,18 @@ public class ZooModule : MonoBehaviour
         };
     }
 
-    private GameObject CreateGraphic(AnimalInfo animal, float x, float y, float z, float scale, Transform parent)
+    private GameObject CreateGraphic(string animal, float x, float y, float z, float scale, Transform parent)
     {
+        Debug.Log("-- Animal texture: " + animal);
         var graphic = Instantiate(Animal);
-        graphic.name = animal.Name;
+        graphic.name = animal;
         graphic.transform.parent = parent;
         graphic.transform.localPosition = new Vector3(x, y, z);
         graphic.transform.localRotation = Quaternion.Euler(0, 180, 0);
         graphic.transform.localScale = new Vector3(scale, scale, scale);
         graphic.AddComponent<MeshFilter>().mesh = PlaneMesh;
         var mr = graphic.AddComponent<MeshRenderer>();
-        var tex = new Texture2D(2, 2);
-        tex.LoadImage(animal.Png);
-        mr.material.mainTexture = tex;
+        mr.material.mainTexture = AnimalImages.First(at => at.name == animal);
         mr.material.shader = Shader.Find("Unlit/Transparent");
         return graphic;
     }
@@ -156,7 +226,7 @@ public class ZooModule : MonoBehaviour
         // Select three out of the five correct animals
         var selection = _solutionLine.ToArray().Shuffle().Take(3).ToList();
         // Select three out of the remaining wrong animals
-        selection.AddRange(Data.Hexes.Values.Where(ai => !_solutionLine.Contains(ai)).ToArray().Shuffle().Take(3));
+        selection.AddRange(_inGrid.Values.Where(ai => !_solutionLine.Contains(ai)).ToArray().Shuffle().Take(3));
         selection.Shuffle();
         _selection = selection.ToArray();
 
@@ -184,13 +254,13 @@ public class ZooModule : MonoBehaviour
                     return false;
                 if (selection[j] != _solutionLine[subprogress])
                 {
-                    Debug.LogFormat("[Zoo #{0}] You pressed {1}, I expected {2}. Strike.", _moduleId, selection[j].Name, _solutionLine[subprogress].Name);
+                    Debug.LogFormat("[Zoo #{0}] You pressed {1}, I expected {2}. Strike.", _moduleId, selection[j], _solutionLine[subprogress]);
                     Module.HandleStrike();
                     _state = State.DoorClosing;
                 }
                 else
                 {
-                    Debug.LogFormat("[Zoo #{0}] {1} was correct.", _moduleId, selection[j].Name);
+                    Debug.LogFormat("[Zoo #{0}] {1} was correct.", _moduleId, selection[j]);
                     do
                         subprogress++;
                     while (subprogress < _solutionLine.Length && !selection.Contains(_solutionLine[subprogress]));
@@ -205,7 +275,7 @@ public class ZooModule : MonoBehaviour
             })(i);
         }
 
-        Debug.LogFormat("[Zoo #{0}] Animals shown behind door: {1}", _moduleId, selection.Select(s => s.Name).JoinString(", "));
+        Debug.LogFormat("[Zoo #{0}] Animals shown behind door: {1}", _moduleId, selection.JoinString(", "));
 
         // SLIDE OPEN
         Audio.PlaySoundAtTransform("SlidingSound", Door.transform);
@@ -257,7 +327,7 @@ public class ZooModule : MonoBehaviour
     {
         if (_animalListMsg1 == null)
         {
-            var animalNames = Data.Hexes.Values.Select(v => v.Name).OrderBy(v => v).ToArray();
+            var animalNames = _inGrid.Values.OrderBy(v => v).ToArray();
             _animalListMsg1 = string.Format("sendtochat {0},", animalNames.Take(animalNames.Length / 2).JoinString(", "));
             _animalListMsg2 = string.Format("sendtochat {0}.", animalNames.Skip(animalNames.Length / 2).JoinString(", ", lastSeparator: " and "));
         }
@@ -282,11 +352,11 @@ public class ZooModule : MonoBehaviour
             yield break;
 
         var animals = m.Groups[1].Value.Split(',').Select(str => str.Trim()).ToArray();
-        var animalInfos = new AnimalInfo[animals.Length];
+        var animalInfos = new KeyValuePair<Hex, string>[animals.Length];
         for (int i = 0; i < animals.Length; i++)
         {
-            animalInfos[i] = Data.Hexes.Values.FirstOrDefault(v => v.Name.Equals(animals[i], StringComparison.InvariantCultureIgnoreCase));
-            if (animalInfos[i] == null)
+            animalInfos[i] = _inGrid.FirstOrDefault(kvp => kvp.Value.Equals(animals[i], StringComparison.InvariantCultureIgnoreCase));
+            if (animalInfos[i].Value == null)
             {
                 yield return string.Format("sendtochat What the hell is a {0}?! I only know about the following animals:", animals[i]);
                 getAnimalListMsgs();
@@ -296,7 +366,7 @@ public class ZooModule : MonoBehaviour
             }
         }
 
-        Debug.LogFormat("[Zoo #{0}] Received Twitch Plays command to press: {1}.", _moduleId, animalInfos.Select(i => i.Name).JoinString(", "));
+        Debug.LogFormat("[Zoo #{0}] Received Twitch Plays command to press: {1}.", _moduleId, animalInfos.Select(i => i.Value).JoinString(", "));
         yield return null;
 
         Door.OnInteract();
@@ -305,7 +375,7 @@ public class ZooModule : MonoBehaviour
         for (int i = 0; i < animalInfos.Length; i++)
         {
             yield return new WaitForSeconds(.1f);
-            var j = Array.IndexOf(_selection, animalInfos[i]);
+            var j = Array.IndexOf(_selection, animalInfos[i].Value);
             if (j == -1)
                 continue;
 
